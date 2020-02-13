be6abb8d43ba4c80853e202c33f8e22f40373d9a
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-02-14 00:08:11 +0400

    Добавление компонентов Quiz и QuizActive

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout";
-// import logo from "./logo.svg";
+import Quiz from "./containers/Quiz";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <div style={{ width: 400, border: "1px solid black" }}>
-          <h1>Layout works</h1>
-        </div>
+        <Quiz />
       </Layout>
     );
   }
