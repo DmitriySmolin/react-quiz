c9395fadd6b9c70b268e7dcf505cd8df9a0ae87d
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-02-14 21:35:50 +0400

    add change of question

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -6,10 +6,12 @@ const ActiveQuiz = props => {
     <div className="ActiveQuiz">
       <p className="Question">
         <span>
-          <strong>2.&nbsp;</strong>
+          <strong>{props.answerNumber}.&nbsp;</strong>
           {props.question}
         </span>
-        <small>4 из 12</small>
+        <small>
+          {props.answerNumber} из {props.quizLength}
+        </small>
       </p>
       <AnswersList
         answers={props.answers}
