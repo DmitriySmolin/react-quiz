ca7e33b3fdf2648569109bce4b5780e3d3c3d2c0
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-02-17 19:42:27 +0400

    update Navigation component

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
 import "./Drawer.css";
 import Backdrop from "../../UI/Backdrop/Backgrop";
-const links = [1, 2, 3];
+import { NavLink } from "react-router-dom";
+const links = [
+  {
+    to: "/",
+    label: "Список",
+    exact: true
+  },
+  {
+    to: "/auth",
+    label: "Авторизация",
+    exact: false
+  },
+  {
+    to: "/quiz-reactor",
+    label: "Создать тест",
+    exact: false
+  }
+];
 
 class Drawer extends Component {
+  clickHandler = () => {
+    this.props.onClose();
+  };
+
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            className="active"
+            onClick={this.clickHandler}
+          >
+            {link.label}
+          </NavLink>
         </li>
       );
     });
