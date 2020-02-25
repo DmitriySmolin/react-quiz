75d04fb3ebf5537e99d41c2973ef25765b25f8ab
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-02-25 22:24:45 +0400

    application authorization page updated via  redux

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,9 +3,10 @@ import "./Auth.css";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 import is from "is_js";
-import axios from "../../axios/axios-quiz";
+import { auth } from "../../store/actions/auth";
+import { connect } from "react-redux";
 
-export default class Auth extends Component {
+class Auth extends Component {
   state = {
     isFormValid: false,
     formControls: {
@@ -36,38 +37,22 @@ export default class Auth extends Component {
     }
   };
 
-  loginHandler = async () => {
-    const authData = {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true
-    };
-    try {
-      const response = await axios.post(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC_QftBucg2t8hAXL-Y3hh2ih2qYTbOeDY",
-        authData
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
+  loginHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      true
+    );
   };
-  registerHandler = async () => {
-    const authData = {
-      email: this.state.formControls.email.value,
-      password: this.state.formControls.password.value,
-      returnSecureToken: true
-    };
-    try {
-      const response = await axios.post(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC_QftBucg2t8hAXL-Y3hh2ih2qYTbOeDY",
-        authData
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
+
+  registerHandler = () => {
+    this.props.auth(
+      this.state.formControls.email.value,
+      this.state.formControls.password.value,
+      true
+    );
   };
+
   submitHandler = e => {
     e.preventDefault();
   };
@@ -155,3 +140,11 @@ export default class Auth extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    auth: (email, password, isLogin) => dispatch(auth(email, password, isLogin))
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
---
+++ src/store/actions/auth.js
@@ -0,0 +1,58 @@
+import axios from "../../axios/axios-quiz";
+import { AUTH_SUCCESS, AUTH_LOGOUT } from "../actions/actionTypes";
+
+export function auth(email, password, isLogin) {
+  return async dispatch => {
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC_QftBucg2t8hAXL-Y3hh2ih2qYTbOeDY";
+
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC_QftBucg2t8hAXL-Y3hh2ih2qYTbOeDY";
+    }
+
+    const response = await axios.post(url, authData);
+    const data = response.data;
+
+    const expirationDate = new Date(
+      new Date().getTime() + data.expiresIn * 1000
+    );
+
+    localStorage.setItem("token", data.idToken);
+    localStorage.setItem("userId", data.localId);
+    localStorage.setItem("expirationDate", expirationDate);
+
+    dispatch(authSucces(data.idToken));
+    dispatch(autoLogout(data.expiresIn));
+  };
+}
+
+export function autoLogout(time) {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, time * 1000);
+  };
+}
+
+export function logout() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("expirationDate");
+  return {
+    type: AUTH_LOGOUT
+  };
+}
+
+export function authSucces(token) {
+  return {
+    type: AUTH_SUCCESS,
+    token
+  };
+}
