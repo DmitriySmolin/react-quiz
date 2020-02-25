db2ca0faa9c552519e6c94a474133d8ab239337a
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-02-25 22:22:21 +0400

    test page updated via redux part1

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import registerServiceWorker from "./registerServiceWorker";
 import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./store/reducers/rootReducer";
-import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+import rootReducer from "./redux/rootReducer";
+
+// function loggerMiddleware(store) {
+//   return function(next) {
+//     return function(action) {
+//       const result = next(action);
+//       console.log("MiddleWare", store.getState());
+//       return result;
+//     };
+//   };
+// }
 
 const composeEnhancers =
   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
     : compose;
+
+const loggerMiddleware = store => next => action => {
+  const result = next(action);
+  console.log("MiddleWare", store.getState());
+  return result;
+};
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk))
 );
+
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>
 );
-ReactDOM.render(app, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(app, document.getElementById("root"));
+registerServiceWorker();
