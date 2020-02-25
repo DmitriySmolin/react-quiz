17df6edbace4eb99567c3f901fec5430b93add72
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-02-25 22:23:13 +0400

    test page updated via redux part2

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import "./App.scss";
-import Counter from "./Counter";
-import { add, sub, addNumber, addAsync } from "./redux/actions/actions";
+import "./App.css";
+import Layout from "./hoc/Layout/Layout";
+import { Switch, Route } from "react-router-dom";
+import Quiz from "./containers/Quiz/Quiz";
+import QuizList from "./containers/QuizList/QuizList";
+import Auth from "./containers/Auth/Auth";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
 
 class App extends Component {
   render() {
-    // console.log("APP", this.props);
     return (
-      <div className={"App"}>
-        <h1>
-          Счетчик <strong>{this.props.counter}</strong>
-        </h1>
-
-        <hr />
-
-        <div className="Actions">
-          <button onClick={this.props.onAdd}>Добавить 1</button>
-          <button onClick={this.props.onSub}>Вычесть 1</button>
-        </div>
-
-        <div className="Actions">
-          <button onClick={this.props.onAddNumber.bind(this, 15)}>
-            Добавить 15
-          </button>
-          <button onClick={this.props.onAddNumber.bind(this, -17)}>
-            Вычесть 17
-          </button>
-        </div>
-        <div className="Actions">
-          <button onClick={this.props.onAddAsync.bind(this, 100)}>
-            Асинхронно добавить 100
-          </button>
-        </div>
-        <Counter />
-      </div>
+      <Layout>
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/quiz-creator" component={QuizCreator} />
+          <Route path="/quiz/:id" component={Quiz} />
+          <Route path="/" component={QuizList} />
+        </Switch>
+      </Layout>
     );
   }
 }
 
-function mapStateToProps(state) {
-  // console.log("State", state);
-  return {
-    counter: state.counter1.counter
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    onAdd: () => dispatch(add()),
-    onSub: () => dispatch(sub()),
-    onAddNumber: number => dispatch(addNumber(number)),
-    onAddAsync: number => dispatch(addAsync(number))
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorker from "./serviceWorker";
 import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import thunk from "redux-thunk";
 import { Provider } from "react-redux";
-import reduxThunk from "redux-thunk";
-import rootReducer from "./redux/rootReducer";
-
-// function loggerMiddleware(store) {
-//   return function(next) {
-//     return function(action) {
-//       const result = next(action);
-//       console.log("MiddleWare", store.getState());
-//       return result;
-//     };
-//   };
-// }
 
 const composeEnhancers =
   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
-
-const loggerMiddleware = store => next => action => {
-  const result = next(action);
-  console.log("MiddleWare", store.getState());
-  return result;
-};
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk))
+  composeEnhancers(applyMiddleware(thunk))
 );
-
 const app = (
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
-
 ReactDOM.render(app, document.getElementById("root"));
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
